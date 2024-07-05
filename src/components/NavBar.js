8b06f68d0d41207a2092275030e975884b87cc7f
Racahel Bull <rachaelbull2021@gmail.com>
2024-07-05 16:31:44 +0000

    Add google fonts import

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -11,15 +11,18 @@ const NavBar = () => {
             </Navbar.Brand>
             <Navbar.Toggle aria-controls="basic-navbar-nav" />
             <Navbar.Collapse id="basic-navbar-nav">
-                <Nav className="mr-auto">
-                    <Nav.Link href="#home">Home</Nav.Link>
-                    <Nav.Link href="#link">Link</Nav.Link>
-                    <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-                        <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-                        <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
-                        <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+                <Nav className="ml-auto text-left">
+                    <Nav.Link><i class="fa-solid fa-house"></i> Home</Nav.Link>
+                    <Nav.Link><i class="fa-solid fa-arrow-right-to-bracket"></i> Sign In</Nav.Link>
+                    <Nav.Link><i class="fa-solid fa-user-plus"></i> Sign Up</Nav.Link>
+                    <NavDropdown title="Information" id="basic-nav-dropdown">
+                        <NavDropdown.Item href="#action/3.1">Getting Married In Italy</NavDropdown.Item>
+                        <NavDropdown.Item href="#action/3.2">Getting Married In The UK</NavDropdown.Item>
+                        <NavDropdown.Item href="#action/3.3">Getting Married In Spain</NavDropdown.Item>
+                        <NavDropdown.Item href="#action/3.3">Getting Married In Greece</NavDropdown.Item>
                         <NavDropdown.Divider />
-                        <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
+                        <NavDropdown.Item href="#action/3.4">Choosing Your Location</NavDropdown.Item>
+                        <NavDropdown.Item href="#action/3.4">Budget Friendly Tips</NavDropdown.Item>
                     </NavDropdown>
                 </Nav>
             </Navbar.Collapse>
