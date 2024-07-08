c46a461e4810848db4f5dac65bce2cb994832596
Racahel Bull <rachaelbull2021@gmail.com>
2024-07-08 12:14:52 +0000

    Add routing to the nav bar and style to information dropdown links

--- src/App.js
+++ src/App.js
@@ -1,10 +1,27 @@
+import styles from "./App.module.css";
 import NavBar from "./components/NavBar";
-import './App.css';
+import Container from "react-bootstrap/Container";
+import { Route, Switch } from "react-router-dom";
+
 
 function App() {
   return (
-    <div className="App">
+    <div className={styles.App}>
        <NavBar />
+       <Container className={styles.Main}>
+        <Switch>
+          <Route exact path="/" render={() => <h1>Home page</h1>} />
+          <Route exact path="/signin" render={() => <h1>Sign in</h1>} />
+          <Route exact path="/signup" render={() => <h1>Sign up</h1>} />
+          <Route exact path="/gettingmarriedinitaly" render={() => <h1>Getting Married In Italy</h1>} />
+          <Route exact path="/gettingmarriedintheuk" render={() => <h1>Getting Married In The UK</h1>} />
+          <Route exact path="/gettingmarriedinspain" render={() => <h1>Getting Married In Spain</h1>} />
+          <Route exact path="/gettingmarriedingreece" render={() => <h1>Getting Married In Greece</h1>} />
+          <Route exact path="/choosingyourlocation" render={() => <h1>Choosing Your Location</h1>} />
+          <Route exact path="/budgetfriendlytips" render={() => <h1>Budget Friendly Tips</h1>} />
+          <Route render={() => <p>Page not found!</p>} />
+        </Switch>
+       </Container>
     </div>
   );
 }
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,33 +1,49 @@
 import React from 'react'
 import { Navbar, Container, Nav, NavDropdown } from "react-bootstrap";
 import logo from "../assets/logo.png";
+import styles from "../styles/NavBar.module.css";
+import { NavLink } from "react-router-dom";
 
 const NavBar = () => {
   return (
-    <Navbar bg="light" expand="md" fixed="top">
+    <Navbar className={styles.NavBar} expand="md" fixed="top">
         <Container>
-            <Navbar.Brand href="#home">
-                <img src={logo} alt="logo" height="45" />
-            </Navbar.Brand>
+            <NavLink to="/">
+                <Navbar.Brand href="#home">
+                    <img src={logo} alt="logo" height="45" />
+                </Navbar.Brand>
+            </NavLink>
             <Navbar.Toggle aria-controls="basic-navbar-nav" />
             <Navbar.Collapse id="basic-navbar-nav">
                 <Nav className="ml-auto text-left">
-                    <Nav.Link><i class="fa-solid fa-house"></i> Home</Nav.Link>
-                    <Nav.Link><i class="fa-solid fa-arrow-right-to-bracket"></i> Sign In</Nav.Link>
-                    <Nav.Link><i class="fa-solid fa-user-plus"></i> Sign Up</Nav.Link>
+                    <NavLink to="/"><i className="fa-solid fa-house"></i> Home</NavLink>
+                    <NavLink to="/signin"><i className="fa-solid fa-arrow-right-to-bracket"></i> Sign In</NavLink>
+                    <NavLink to="/signup"><i className="fa-solid fa-user-plus"></i> Sign Up</NavLink>
                     <NavDropdown title="Information" id="basic-nav-dropdown">
-                        <NavDropdown.Item href="#action/3.1">Getting Married In Italy</NavDropdown.Item>
-                        <NavDropdown.Item href="#action/3.2">Getting Married In The UK</NavDropdown.Item>
-                        <NavDropdown.Item href="#action/3.3">Getting Married In Spain</NavDropdown.Item>
-                        <NavDropdown.Item href="#action/3.3">Getting Married In Greece</NavDropdown.Item>
+                        <NavDropdown.Item>
+                            <NavLink to="/gettingmarriedinitaly">Getting Married In Italy</NavLink>
+                        </NavDropdown.Item>
+                        <NavDropdown.Item>
+                            <NavLink to="/gettingmarriedintheuk">Getting Married In The UK</NavLink>
+                        </NavDropdown.Item>
+                        <NavDropdown.Item>
+                            <NavLink to="/gettingmarriedinspain">Getting Married In Spain</NavLink>
+                        </NavDropdown.Item>
+                        <NavDropdown.Item>
+                            <NavLink to="/gettingmarriedingreece">Getting Married In Greece</NavLink>
+                        </NavDropdown.Item>
                         <NavDropdown.Divider />
-                        <NavDropdown.Item href="#action/3.4">Choosing Your Location</NavDropdown.Item>
-                        <NavDropdown.Item href="#action/3.4">Budget Friendly Tips</NavDropdown.Item>
+                        <NavDropdown.Item>
+                            <NavLink to="/choosingyourlocation">Choosing Your Location</NavLink>
+                        </NavDropdown.Item>
+                        <NavDropdown.Item>
+                            <NavLink to="/budgetfriendlytips">Budget Friendly Tips</NavLink>
+                        </NavDropdown.Item>
                     </NavDropdown>
                 </Nav>
             </Navbar.Collapse>
         </Container>
-</Navbar>
+    </Navbar>
   )
 };
 
