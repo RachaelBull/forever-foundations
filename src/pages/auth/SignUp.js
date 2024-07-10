47676064a4ad08cf6220358f5568441b938660ed
Racahel Bull <rachaelbull2021@gmail.com>
2024-07-10 17:34:31 +0000

    Start to style the sign up form

--- src/pages/auth/SignUp.js
+++ src/pages/auth/SignUp.js
@@ -2,22 +2,44 @@ import React from "react";
 import { Link } from "react-router-dom";
 import signuppic from "../../assets/sign-up-pic.jpg"
 import appStyles from "../../App.module.css";
+import styles from "../../styles/SignInUpForm.module.css";
+import btnStyles from "../../styles/Button.module.css";
 
-import { Image, Col, Row, Container } from "react-bootstrap";
+import { Button, Form, Image, Col, Row, Container } from "react-bootstrap";
 
 const SignUpForm = () => {
   return (
     <Row>
       <Col className="my-auto py-2 p-md-2" md={6}>
         <Container className= "p-4">
-          <h1>sign up</h1>
+          <h1 className={styles.header}>Sign Up</h1>
 
-            {/* add your form here */}
+          <Form>
+            <Form.Group controlId="formBasicEmail">
+                <Form.Label className="d-none">Username</Form.Label>
+                <Form.Control className={styles.input} type="text" placeholder="Enter Username" name="username" />
+            </Form.Group>
+
+            <Form.Group controlId="password1">
+                <Form.Label className="d-none">Password</Form.Label>
+                <Form.Control className={styles.input} type="password" placeholder="Password" name="password1" />
+            </Form.Group>
+            <Form.Group controlId="password2">
+                <Form.Label className="d-none">Confirm Password</Form.Label>
+                <Form.Control className={styles.input} type="password" placeholder="Confirm Password" name="password2" />
+            </Form.Group>
+            <Form.Group controlId="formBasicCheckbox">
+                <Form.Check className={styles.input} type="checkbox" label="Create an account for a business" />
+            </Form.Group>
+            <Button type="submit" className={btnStyles.signupbtn}>
+            Sign Up
+            </Button>
+          </Form>
 
         </Container>
         <Container>
-          <Link to="/signin">
-            Already have an account? <span>Sign in</span>
+          <Link className={styles.Link} to="/signin">
+            Already have an account? <span>Sign In</span>
           </Link>
         </Container>
       </Col>
@@ -26,7 +48,7 @@ const SignUpForm = () => {
         className="my-auto d-none d-md-block p-2"
       >
         <Image className={`${appStyles.FillerImage}`}
-          src={signuppic}
+          src={signuppic} alt="two gold wedding rings"
         />
       </Col>
     </Row>
