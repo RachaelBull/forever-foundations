6a5ebacb8800f85cecb3bfe0f0532df89d6e8b5c
Racahel Bull <rachaelbull2021@gmail.com>
2024-07-12 12:22:14 +0000

    Sign in page and form styled and responsive

--- src/pages/auth/SignIn.js
+++ src/pages/auth/SignIn.js
@@ -12,8 +12,32 @@ import { Button, Form, Image, Col, Row, Container, Alert} from "react-bootstrap"
 const SignInForm = () => {
     return (
         <div className={styles.background} style={{ backgroundImage: `url(${signinbg})`}}>
-          <h1 className={styles.signinhead}>Sign In Page</h1>
-        </div>
+          <Row className={styles.Row}>
+      <Col className="my-auto p-0 p-md-2" md={6}>
+        <Container className={`${styles.signincontainer} p-4 `}>
+          <h1 className={styles.header}>sign in</h1>
+          <Form>
+            <Form.Group controlId="formBasicEmail">
+              <Form.Label className="d-none">Username</Form.Label>
+              <Form.Control className={styles.input} type="text" placeholder="Username" />
+            </Form.Group>
+
+            <Form.Group controlId="formBasicPassword">
+              <Form.Label className="d-none">Password</Form.Label>
+              <Form.Control className={styles.input} type="password" placeholder="Password" />
+            </Form.Group>
+            <Button variant="primary" type="submit" className={btnStyles.signupbtn}>
+              Sign In
+            </Button>
+            <Link className={styles.Link} to="/signup">
+            Don't have an account yet? <span>Sign up.</span>
+          </Link>
+          </Form>
+
+        </Container>
+      </Col>
+    </Row>
+    </div>
       );
     }
     
