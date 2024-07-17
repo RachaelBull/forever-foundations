db412cbb82a5160ba406c5eb49090a416c8e834d
Racahel Bull <rachaelbull2021@gmail.com>
2024-07-17 18:22:47 +0000

    Start the post upload form and styling

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { Route, Switch } from "react-router-dom";
 import './api/axiosDefaults';
 import SignUpForm from "./pages/auth/SignUp";
 import SignInForm from "./pages/auth/SignIn";
+import PostCreateForm from "./pages/posts/PostCreateForm";
 
 
 function App() {
@@ -15,6 +16,7 @@ function App() {
               <Route exact path="/" render={() => <h1>Home page</h1>} />
               <Route exact path="/signin" render={() => <SignInForm />} />
               <Route exact path="/signup" render={() => <SignUpForm />} />
+              <Route exact path="/posts/create" render={() => <PostCreateForm />} />
               <Route exact path="/gettingmarriedinitaly" render={() => <h1>Getting Married In Italy</h1>} />
               <Route exact path="/gettingmarriedintheuk" render={() => <h1>Getting Married In The UK</h1>} />
               <Route exact path="/gettingmarriedinspain" render={() => <h1>Getting Married In Spain</h1>} />
