97a9f72815742cf9508c250a31d58a750ee77862
Racahel Bull <rachaelbull2021@gmail.com>
2024-07-23 15:40:31 +0000

    Implement the spinner component and start the search styling

--- src/pages/posts/PostsPage.js
+++ src/pages/posts/PostsPage.js
@@ -1,21 +1,60 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 import Form from "react-bootstrap/Form";
 import Col from "react-bootstrap/Col";
 import Row from "react-bootstrap/Row";
 import Container from "react-bootstrap/Container";
 
-import appStyles from "../../App.module.css";
+import Post from "./Post";
+import Asset from "../../components/Asset";
+import NoResults from "../../assets/no-results.png";
+
+import { useLocation } from "react-router";
+import { axiosReq } from "../../api/axiosDefaults";
 import styles from "../../styles/Post.module.css";
 
-function PostsPage() {
+function PostsPage({ message, filter = "" }) {
+    const [posts, setPosts] = useState({ results: [] });
+    const [hasLoaded, setHasLoaded] = useState(false);
+    const { pathname } = useLocation();
+
+    useEffect(() => {
+        const fetchPosts = async () => {
+          try {
+            const { data } = await axiosReq.get(`/posts/?${filter}`);
+            setPosts(data);
+            setHasLoaded(true);
+          } catch (err) {
+            console.log(err);
+          }
+        };
+    
+        setHasLoaded(false);
+        fetchPosts();
+      }, [filter, pathname]);
   
   return (
     <Container>
         <Row className={`${styles.postcontainer} h-100 `}>
             <Col className="py-2 p-0 p-lg-2" lg={8}>
             <p>Popular profiles mobile</p>
-            <p>List of posts here</p>
+            {hasLoaded ? (
+             <>
+            {posts.results.length ? (
+              posts.results.map((post) => (
+                <Post key={post.id} {...post} setPosts={setPosts} />
+              ))
+            ) : (
+              <Container>
+                <Asset src={NoResults} message={message} />
+              </Container>
+            )}
+          </>
+        ) : (
+          <Container>
+            <Asset spinner />
+          </Container>
+        )}
             </Col>
             <Col md={4} className="d-none d-lg-block p-0 p-lg-2">
             <p>Popular profiles for desktop</p>
