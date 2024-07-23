c89deefbffafd11d4611c719cbea790a2bae89df
Racahel Bull <rachaelbull2021@gmail.com>
2024-07-23 15:11:34 +0000

    Add routing for home, loved and feed

--- src/App.js
+++ src/App.js
@@ -6,15 +6,21 @@ import SignUpForm from "./pages/auth/SignUp";
 import SignInForm from "./pages/auth/SignIn";
 import PostCreateForm from "./pages/posts/PostCreateForm";
 import PostPage from "./pages/posts/PostPage";
+import PostsPage from "./pages/posts/PostsPage";
+import { useCurrentUser } from "./contexts/CurrentUserContext";
 
 
 function App() {
+  const currentUser = useCurrentUser();
+  const profile_id = currentUser?.profile_id || "";
 
   return (
         <div className={styles.App}>
           <NavBar />
             <Switch>
-              <Route exact path="/" render={() => <h1>Home page</h1>} />
+            <Route exact path="/" render={() => (<PostsPage message="No results found. Please search using keywords.." />)}/>
+              <Route exact path="/feed" render={() => (<PostsPage message="No results found. Adjust the search keyword or follow a user." filter={`owner__followed__owner__profile=${profile_id}&`}/>)}/>
+              <Route exact path="/loved" render={() => (<PostsPage message="No results found. Adjust the search keyword or love a post." filter={`likes__owner__profile=${profile_id}&ordering=-likes__created_on&`}/>)}/>
               <Route exact path="/signin" render={() => <SignInForm />} />
               <Route exact path="/signup" render={() => <SignUpForm />} />
               <Route exact path="/posts/create" render={() => <PostCreateForm />} />
---
+++ src/pages/posts/PostsPage.js
@@ -0,0 +1,28 @@
+import React from "react";
+
+import Form from "react-bootstrap/Form";
+import Col from "react-bootstrap/Col";
+import Row from "react-bootstrap/Row";
+import Container from "react-bootstrap/Container";
+
+import appStyles from "../../App.module.css";
+import styles from "../../styles/Post.module.css";
+
+function PostsPage() {
+  
+  return (
+    <Container>
+        <Row className={`${styles.postcontainer} h-100 `}>
+            <Col className="py-2 p-0 p-lg-2" lg={8}>
+            <p>Popular profiles mobile</p>
+            <p>List of posts here</p>
+            </Col>
+            <Col md={4} className="d-none d-lg-block p-0 p-lg-2">
+            <p>Popular profiles for desktop</p>
+            </Col>
+        </Row>
+    </Container>
+  );
+}
+
+export default PostsPage;
