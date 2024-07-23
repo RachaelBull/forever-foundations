bb2b2dd805b5f3de1764b737c01fcf7da945eac0
Racahel Bull <rachaelbull2021@gmail.com>
2024-07-23 16:14:25 +0000

    Install and impliment the infinite scroll component

--- src/pages/posts/PostsPage.js
+++ src/pages/posts/PostsPage.js
@@ -8,6 +8,8 @@ import Container from "react-bootstrap/Container";
 import Post from "./Post";
 import Asset from "../../components/Asset";
 import NoResults from "../../assets/no-results.png";
+import InfiniteScroll from "react-infinite-scroll-component";
+import { fetchMoreData } from "../../utils/utils";
 
 import { useLocation } from "react-router";
 import { axiosReq } from "../../api/axiosDefaults";
@@ -18,10 +20,12 @@ function PostsPage({ message, filter = "" }) {
     const [hasLoaded, setHasLoaded] = useState(false);
     const { pathname } = useLocation();
 
+    const [query, setQuery] = useState("");
+
     useEffect(() => {
         const fetchPosts = async () => {
           try {
-            const { data } = await axiosReq.get(`/posts/?${filter}`);
+            const { data } = await axiosReq.get(`/posts/?${filter}search=${query}`);
             setPosts(data);
             setHasLoaded(true);
           } catch (err) {
@@ -30,20 +34,46 @@ function PostsPage({ message, filter = "" }) {
         };
     
         setHasLoaded(false);
-        fetchPosts();
-      }, [filter, pathname]);
+        const timer = setTimeout(() => {
+          fetchPosts();
+        }, 1000);
+    
+        return () => {
+          clearTimeout(timer);
+        };
+      }, [filter, query, pathname]);
   
   return (
     <Container>
         <Row className={`${styles.postcontainer} h-100 `}>
             <Col className="py-2 p-0 p-lg-2" lg={8}>
             <p>Popular profiles mobile</p>
+            <i className={`fas fa-search ${styles.searchicon}`} />
+            <Form
+                className={styles.SearchBar}
+                onSubmit={(event) => event.preventDefault()}
+            >
+            <Form.Control
+                value={query}
+                onChange={(event) => setQuery(event.target.value)}
+                type="text"
+                className="mr-sm-2"
+                placeholder="Search posts"
+            />
+            </Form>
+
             {hasLoaded ? (
              <>
             {posts.results.length ? (
-              posts.results.map((post) => (
+                <InfiniteScroll
+                children={posts.results.map((post) => (
                 <Post key={post.id} {...post} setPosts={setPosts} />
-              ))
+              ))}
+              dataLength={posts.results.length}
+                loader={<Asset spinner />}
+                hasMore={!!posts.next}
+                next={() => fetchMoreData(posts, setPosts)}
+              />
             ) : (
               <Container>
                 <Asset src={NoResults} message={message} />
