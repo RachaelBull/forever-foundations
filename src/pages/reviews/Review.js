19f29103375648ae4d4e7acf70f439fb746d8d76
Racahel Bull <rachaelbull2021@gmail.com>
2024-08-11 17:02:03 +0000

    Add handle delete functionality for the reviews

--- src/pages/reviews/Review.js
+++ src/pages/reviews/Review.js
@@ -5,14 +5,33 @@ import { Media } from "react-bootstrap";
 import ReviewEditForm from './ReviewEditForm';
 import { useCurrentUser } from "../../contexts/CurrentUserContext";
 import { MoreDropdown } from "../../components/MoreDropdown";
+import { axiosRes } from "../../api/axiosDefaults";
 
 const Review = (props) => {
-  const { profile_id, profile_image, owner, created_on, content, setReviews, id } = props;
+  const { profile_id, profile_image, owner, created_on, content, setReviews, id, setProfile } = props;
 
   const [showEditForm, setShowEditForm] = useState(false);
 
   const currentUser = useCurrentUser();
   const is_owner = currentUser?.username === owner;
+
+  const handleDelete = async () => {
+    try {
+      await axiosRes.delete(`/reviews/${id}/`);
+      setProfile((prevProfile) => ({
+        results: [
+          {
+            ...prevProfile.results[0]
+          },
+        ],
+      }));
+
+      setReviews((prevReviews) => ({
+        ...prevReviews,
+        results: prevReviews.results.filter((review) => review.id !== id),
+      }));
+    } catch (err) {}
+  };
 
   return (
     <div>
@@ -47,6 +66,7 @@ const Review = (props) => {
         {!is_owner && !showEditForm && (
           <MoreDropdown
             handleEdit={() => setShowEditForm(true)}
+            handleDelete={handleDelete}
           />
         )}
       </Media>
